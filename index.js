c194b5997e2682f669cbe0f7e36b61565754d285
matthewdavi <maathewdavis@gmail.com>
2017-07-22 01:08:22 -0400

    got it to work for one user

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
-var Twitter = require('twitter-node-client').Twitter;
-const config = require('./config.js');
+//@flow
+const Twitter = require(`twitter`);
+const client = require(`./config`);
+const sentiment = require(`sentiment`);
+
+function readTweets(tweets, array){
+ 	let lastID;
+ 	tweets.forEach( function(tweet,index){
+ 		if(index ===0){
+ 			lastID = tweet.id
+ 		}
+ 		else if(tweet.id < lastID){
+ 			lastID = tweet.id;
+ 		}
+ 			tweetArray.push(tweet)
+ 			//console.log(tweetArray.length, tweet.text)
+
+ 		
+ })
+ 		  	return Number(lastID)-1;
+
+}
+
+const getParams = function(max_id, username){
+	if (max_id === null || max_id === undefined){
+		return {screen_name:username, count: 200};
+	}
+
+	return {screen_name:username, count: 200, max_id: max_id};
+}
+
+	const getTweets = function(params){
+		client.get(`statuses/user_timeline`, params).then(function(tweets){
+			new Promise(reject, resolve) =>
+
+
+				let last = readTweets(tweets, []);
+				getTweets(getParams(last, params.screen_name))
+				resolve(tweets)
+		}).catch((error) => console.log(error))
+
+	}
+
+getTweets(getParams(null, `mtthwdvs`))
+
+const getSentiment = function(tweets){
+	let score = 0;
+	tweets.forEach(function(tweet, index){
+		score = score + sentiment(tweet).score
+		console.log(index)
+	})
+	console.log("this is your score: ", score)
+}
+
+
